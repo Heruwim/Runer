6c208dfec904f3323a8258905c39354930300067
Heruwim <heruwim25@gmail.com>
2023-03-13 20:32:12 +0200

    Added new health bar

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int _helth;
 
+    public event UnityAction<int> HealthChanged;
+    public event UnityAction Died;
+
+    private void Start()
+    {
+        HealthChanged.Invoke(_helth);
+    }
+
     public void ApplyDamage(int damage)
     {
         _helth -= damage;
+        HealthChanged?.Invoke(_helth);
 
         if (_helth <= 0)
             Die();
@@ -14,6 +24,6 @@ public class Player : MonoBehaviour
 
     public void Die()
     {
-
+        Died?.Invoke();        
     }
 }
